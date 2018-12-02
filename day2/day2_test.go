15164f7a91eb860bc2493d73a14312c374e4bc1d
Pietro Menna <pietromenna@gmail.com>
2018-12-02 19:34:36 -0200

    Day 2 p2

--- day2/day2_test.go
+++ day2/day2_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Ct struct {
-	two int
+	two   int
 	three int
 }
 
@@ -19,7 +19,7 @@ func Test_CountTwosOrThrees(t *testing.T) {
 	}{
 		{
 			"abcdef",
-			Ct{two:0, three: 0},
+			Ct{two: 0, three: 0},
 		},
 		{
 			"bababc",
@@ -54,21 +54,24 @@ func Test_CountTwosOrThrees(t *testing.T) {
 	for _, tc := range tests {
 		result := Count(tc.in)
 		if result.two != tc.out.two || result.three != tc.out.three {
-			t.Errorf("%v is different to %v",result, tc )
+			t.Errorf("%v is different to %v", result, tc)
 		}
 	}
 
 }
 
-func Test_Part1ChecksumExample(t *testing.T){
-	example := []string{"abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab",}
+func Test_Part1ChecksumExample(t *testing.T) {
+	example := []string{"abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"}
 
 	if CheckSum(example) != 12 {
 		t.Fail()
 	}
 }
 
-func Test_Part1(t *testing.T){
+type CtUnique struct {
+}
+
+func Test_Part1(t *testing.T) {
 	filePath := "/Users/pfm/go/src/github.com/pietromenna/aoc2018/day2/input.txt"
 	dat, _ := ioutil.ReadFile(filePath)
 
@@ -76,10 +79,10 @@ func Test_Part1(t *testing.T){
 	input := strings.Split(inputInString, "\n")
 
 	fmt.Printf("%d", CheckSum(input))
-		t.Fail()
+	t.Fail()
 }
 
-func CheckSum(strings []string) int{
+func CheckSum(strings []string) int {
 	two := 0
 	three := 0
 
@@ -88,7 +91,7 @@ func CheckSum(strings []string) int{
 		if r.two > 0 {
 			two += 1
 		}
-		if r.three > 0{
+		if r.three > 0 {
 			three += 1
 		}
 	}
@@ -99,7 +102,7 @@ func Count(s string) (result Ct) {
 	result.two = 0
 	result.three = 0
 	counter := make(map[rune]int)
-	for _,c := range s {
+	for _, c := range s {
 		if _, ok := counter[c]; !ok {
 			counter[c] = 1
 		} else {
@@ -107,7 +110,7 @@ func Count(s string) (result Ct) {
 		}
 	}
 
-	for _,v := range counter {
+	for _, v := range counter {
 		if v == 2 {
 			result.two += 1
 		}
@@ -117,3 +120,82 @@ func Count(s string) (result Ct) {
 	}
 	return
 }
+
+func CountUnique(in string) int {
+	unique := 0
+	counter := make(map[rune]int)
+	for _, c := range in {
+		if _, ok := counter[c]; !ok {
+			counter[c] = 1
+		} else {
+			counter[c] += 1
+		}
+	}
+
+	for _, v := range counter {
+		if v == 1 {
+			unique += 1
+		}
+	}
+	return unique
+}
+func Test_Part2Example(t *testing.T) {
+	example := []string{"abcde",
+		"fghij",
+		"klmno",
+		"pqrst",
+		"fguij",
+		"axcye",
+		"wvxyz"}
+
+	for _, outer := range example {
+		for _, inner := range example {
+			if inner != outer {
+				if CountUnique(outer+inner) == 2 {
+					fmt.Printf("%s and %s \n", outer, inner)
+					fmt.Println(AnsFromStrings(outer, inner))
+					t.Fail()
+				}
+			}
+		}
+	}
+	t.Fail()
+}
+
+func Test_Part2(t *testing.T) {
+	filePath := "/Users/pfm/go/src/github.com/pietromenna/aoc2018/day2/input.txt"
+	dat, _ := ioutil.ReadFile(filePath)
+
+	inputInString := string(dat)
+	input := strings.Split(inputInString, "\n")
+
+	for _, outer := range input {
+		for _, inner := range input {
+			if inner != outer {
+				if AnsFromStrings(outer, inner) != "" {
+					fmt.Printf("%s and %s \n", outer, inner)
+					fmt.Println(AnsFromStrings(outer, inner))
+					t.Fail()
+				}
+			}
+		}
+	}
+	t.Fail()
+}
+
+func AnsFromStrings(s1, s2 string) string {
+	ans := ""
+	diff := 0
+	for i, _ := range s1{
+		if diff >= 2 {
+			return ""
+		}
+		if s1[i] == s2[i] {
+			ans = ans + string(s1[i])
+		} else {
+			diff += 1
+		}
+	}
+	return ans
+}
+
