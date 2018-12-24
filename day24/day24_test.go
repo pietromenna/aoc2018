b111cf9e1c43327c13d32bbae1a5be389c5d59f4
Pietro Menna <pietromenna@gmail.com>
2018-12-24 14:17:24 -0200

    Day 24 p1 not there yet. Saving not to loose uber

--- day24/day24_test.go
+++ day24/day24_test.go
@@ -1,42 +1,46 @@
 package day24
 
 import (
+	"io/ioutil"
 	"sort"
+	"strconv"
+	"strings"
 	"testing"
 )
 
 type Damage struct {
-	Value int
+	Value      int
 	Initiative int
-	Type string
+	Type       string
 }
 
 type Unit struct {
-	HitPoints int
+	HitPoints  int
 	Immunities []string
-	WeakTo []string
-	Damage Damage
+	WeakTo     []string
+	Damage     Damage
 }
 
 type Group struct {
-	UnitCount int
-	Unit Unit
+	UnitCount  int
+	Unit       Unit
 	Initiative int
-	Type string
+	Type       string
 }
 
 type Simulation struct {
-	Infection []*Group
+	Infection    []*Group
 	ImmuneSystem []*Group
 }
 
 type Target struct {
 	Attacking *Group
 	Defending *Group
-	Damage int
+	Damage    int
 }
 
 type byBestPlan []Target
+
 func (s byBestPlan) Len() int {
 	return len(s)
 }
@@ -67,6 +71,7 @@ func (s byEffectivePower) Less(i, j int) bool {
 }
 
 type byInitiative []*Group
+
 func (s byInitiative) Len() int {
 	return len(s)
 }
@@ -78,7 +83,6 @@ func (s byInitiative) Swap(i, j int) {
 func (s byInitiative) Less(i, j int) bool {
 	return s[i].Initiative > s[j].Initiative
 }
-
 
 func (g *Group) ComputeTotalDamage(d Group) int {
 	if g.Type == d.Type {
@@ -115,11 +119,11 @@ func (g *Group) TakeDamage(damage int) {
 		g.UnitCount = 0
 		return
 	}
-	g.UnitCount = ((canTakeDamage - damage)/ g.Unit.HitPoints ) + 1
+	g.UnitCount = ((canTakeDamage - damage) / g.Unit.HitPoints) + 1
 }
 
-func GetPlansFor(g *Group, inTargets []Target) []Target{
-	targets := make([]Target,0)
+func GetPlansFor(g *Group, inTargets []Target) []Target {
+	targets := make([]Target, 0)
 	for _, t := range inTargets {
 		if t.Attacking == g {
 			targets = append(targets, t)
@@ -144,9 +148,9 @@ func (s *Simulation) RemainingUnits() int {
 }
 
 func (s *Simulation) Winner() *Group {
-	if len(s.ImmuneSystem) > 0 && len(s.Infection) == 0{
+	if len(s.ImmuneSystem) > 0 && len(s.Infection) == 0 {
 		return s.ImmuneSystem[0]
-	} else if len(s.Infection) > 0 && len(s.ImmuneSystem) == 0{
+	} else if len(s.Infection) > 0 && len(s.ImmuneSystem) == 0 {
 		return s.Infection[0]
 	}
 	return nil
@@ -154,7 +158,7 @@ func (s *Simulation) Winner() *Group {
 
 func (s *Simulation) Tick() {
 	// target selection
-	groups := make([]*Group,0)
+	groups := make([]*Group, 0)
 	for _, g := range s.ImmuneSystem {
 		groups = append(groups, g)
 	}
@@ -162,12 +166,12 @@ func (s *Simulation) Tick() {
 		groups = append(groups, g)
 	}
 	sort.Sort(byEffectivePower(groups))
-	targetPlans := make([]Target,0)
+	targetPlans := make([]Target, 0)
 	for _, g := range groups {
 		for _, d := range groups {
-			targetPlan := Target{g,d,g.ComputeTotalDamage(*d)}
+			targetPlan := Target{g, d, g.ComputeTotalDamage(*d)}
 			if targetPlan.Damage != 0 {
-				targetPlans = append(targetPlans,targetPlan)
+				targetPlans = append(targetPlans, targetPlan)
 			}
 		}
 	}
@@ -186,7 +190,7 @@ func (s *Simulation) Tick() {
 }
 
 func CleanTargetPlans(tp []Target) []Target {
-	targets := make([]Target,0)
+	targets := make([]Target, 0)
 	attackersVisited := make(map[*Group]bool)
 	defenderVisited := make(map[*Group]bool)
 	for _, t := range tp {
@@ -202,7 +206,7 @@ func CleanTargetPlans(tp []Target) []Target {
 }
 
 func ReturnAlive(groups []*Group) []*Group {
-	alive := make([]*Group,0)
+	alive := make([]*Group, 0)
 	for _, g := range groups {
 		if g.UnitCount > 0 {
 			alive = append(alive, g)
@@ -223,19 +227,115 @@ func Test_PartOneExample(t *testing.T) {
 	}
 }
 
+func Test_PartOne(t *testing.T) {
+	filePath := "/Users/pfm/go/src/github.com/pietromenna/aoc2018/day24/input.txt"
+	dat, _ := ioutil.ReadFile(filePath)
+
+	input := string(dat)
+	simulation := CreateSimulationFromInput(input)
+
+	for simulation.Winner() == nil {
+		simulation.Tick()
+	}
+
+	if simulation.RemainingUnits() != 5216 {
+		t.Errorf("Expected: %d, Got: %d", 5216, simulation.RemainingUnits())
+	}
+}
+
 func CreateExampleSimulation() *Simulation {
 	simulation := Simulation{}
 	//immune system
 	immuneUnit1 := Unit{5390, make([]string, 0), []string{"radiation", "bludgeoning"}, Damage{4507, 2, "fire"}}
-	immuneGroup1 := Group{17, immuneUnit1, 2,"Immune"}
-	immuneUnit2 := Unit{1274, []string{"fire"}, []string{"bludgeoning","slashing"}, Damage{25, 3, "slashing"}}
-	immuneGroup2 := Group{989, immuneUnit2, 3,"Immune"}
+	immuneGroup1 := Group{17, immuneUnit1, 2, "Immune"}
+	immuneUnit2 := Unit{1274, []string{"fire"}, []string{"bludgeoning", "slashing"}, Damage{25, 3, "slashing"}}
+	immuneGroup2 := Group{989, immuneUnit2, 3, "Immune"}
 	simulation.ImmuneSystem = []*Group{&immuneGroup1, &immuneGroup2}
 	//infection
 	infectionUnit1 := Unit{4706, make([]string, 0), []string{"radiation"}, Damage{116, 1, "bludgeoning"}}
-	infectionGroup1 := Group{801, infectionUnit1, 1,"Infection"}
+	infectionGroup1 := Group{801, infectionUnit1, 1, "Infection"}
 	infectionUnit2 := Unit{2961, []string{"radiation"}, []string{"fire", "cold"}, Damage{12, 4, "slashing"}}
-	infectionGroup2 := Group{4485, infectionUnit2,4,"Infection"}
+	infectionGroup2 := Group{4485, infectionUnit2, 4, "Infection"}
 	simulation.Infection = []*Group{&infectionGroup1, &infectionGroup2}
 	return &simulation
+}
+
+func CreateSimulationFromInput(in string) *Simulation {
+	simulation := Simulation{}
+	simulation.Infection = make([]*Group, 0)
+	simulation.ImmuneSystem = make([]*Group, 0)
+
+	lines := strings.Split(in, "\n")
+	current := ""
+	for _, l := range lines {
+		if strings.Contains(l, "Immune System:") {
+			current = "Immune System:"
+		} else if strings.Contains(l, "Infection:") {
+			current = "Infection:"
+		} else {
+			if l != ""{
+				group := ParseLine(l)
+
+				if current == "Immune System:" {
+					group.Type = "immune"
+					simulation.ImmuneSystem = append(simulation.ImmuneSystem, &group)
+				} else {
+					group.Type = "infection"
+					simulation.Infection = append(simulation.Infection, &group)
+				}
+			}
+		}
+	}
+
+	return &simulation
+}
+
+func ParseLine(in string) Group {
+	g := Group{}
+	u := Unit{}
+	d := Damage{}
+	u.Immunities = make([]string, 0)
+	u.WeakTo = make([]string, 0)
+	tokens := strings.Split(in, " ")
+	g.UnitCount, _ = strconv.Atoi(tokens[0])
+	u.HitPoints, _ = strconv.Atoi(tokens[4])
+
+	//get damage
+	parts := strings.Split(in, "with an attack that does ")
+	damageTokens := strings.Split(parts[1], " ")
+	d.Value, _ = strconv.Atoi(damageTokens[0])
+	d.Initiative, _ = strconv.Atoi(damageTokens[5])
+	d.Type = damageTokens[1]
+	g.Initiative = d.Initiative
+	u.Damage = d
+
+	for i, t := range tokens {
+		if strings.Contains(t, "weak") {
+			u.WeakTo = GetSpecials(tokens[i+2:])
+		}
+
+		if strings.Contains(t, "immune") {
+			u.Immunities = GetSpecials(tokens[i+2:])
+		}
+	}
+
+	g.Unit = u
+	return g
+}
+
+func GetSpecials(in []string) []string {
+	specials := make([]string,0)
+	for _, s := range in {
+		if strings.Contains(s, ")") || strings.Contains(s,";") {
+			a := strings.Replace(s, ")", "",1)
+			a = strings.Replace(a, ";", "",1)
+			specials = append(specials, a)
+			break
+		} else if strings.Contains(s, ",") {
+			a := strings.Replace(s, ",", "",1)
+			specials = append(specials, a)
+		}
+
+	}
+	return specials
 }
